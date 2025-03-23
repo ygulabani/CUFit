57251bd76e2a63bee87dae8dd9dd002e16c3b296
Hamid-Bashyan <hamid.bashyan.1989@gmail.com>
2025-03-22 21:33:12 -0400

    Final working version with exercise filtering and auth bug fix

--- cufit/workout/urls.py
+++ cufit/workout/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import get_user_profile
 
 urlpatterns = [
     path('api/exercises/', views.get_exercises, name='get_exercises'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('api/user-workout/', views.get_user_workout, name='get_user_workout'),
     path('api/update-exercise-routine/', views.update_exercise_routine, name='update_exercise_routine'),
     path('api/save-equipment/', views.save_equipment, name='save-equipment'),
+    path('get-profile/', get_user_profile, name='get_user_profile'),
 ]
