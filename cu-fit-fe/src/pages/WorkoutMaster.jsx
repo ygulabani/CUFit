d91081bc9a182598e54fecad7d23f8bfbba10332
Harsh-patel27 <harshmaheshbhai.patel@mail.concordia.ca>
2025-04-03 17:13:08 -0400

    update color

--- cu-fit-fe/src/pages/WorkoutMaster.jsx
+++ cu-fit-fe/src/pages/WorkoutMaster.jsx
@@ -103,7 +103,7 @@ export default function WorkoutMaster() {
 
                 <button
                   onClick={() => setSelectedVideo(workout.video_link)}
-                  className="w-full bg-purple-500 text-white py-2 rounded-lg hover:bg-purple-600 transition-colors duration-200 flex items-center justify-center"
+                  className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition-colors duration-200 flex items-center justify-center"
                 >
                   <svg
                     className="w-5 h-5 mr-2"
